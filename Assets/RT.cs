400feaaef71630e34f38c3ec67805bb444afa4c5
Shiina Manatus <whitesky@live.cn>
2020-04-28 15:34:42 +0800

    UI Method

---
+++ Assets/RT.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+[ExecuteInEditMode]
+public class RT : MonoBehaviour
+{
+    [Range(0,1)]
+    /// <summary>
+    /// Percentage for the renderer
+    /// </summary>
+    public float percentage = 1f;
+
+    public void FixedUpdate()
+    {
+        var renderer = GetComponent<CanvasRenderer>();
+        renderer.SetAlpha(percentage);
+    }
+}
