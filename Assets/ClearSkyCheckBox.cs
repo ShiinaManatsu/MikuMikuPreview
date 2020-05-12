9c40ff2913cfa174db8fa9fdc779f766e05bf2c6
Shiina Manatus <whitesky@live.cn>
2020-05-12 14:58:34 +0800

    UI change

---
+++ Assets/ClearSkyCheckBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+using UnityEngine.Experimental.UIElements;
+
+public class ClearSkyCheckBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var toggle = GetComponent<Toggle>();
+        GetComponent<Volume>().profile.TryGet(out VisualEnvironment enviroment);
+        Observable.EveryUpdate()
+            .Select(_ => toggle.value)
+            .DistinctUntilChanged()
+            .Subscribe(x => enviroment.active = !x);
+    }
+}
